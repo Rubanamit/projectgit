5d16ac1ebdfbc5949ccce4887e8ba8990315365c
Rubanamit <rubanpati198@gmail.com>
2022-09-23 14:49:00 +0530

    Ruban: Pushing projectgit to github repository

---
+++ projectgit/src/test/java/gitrepository/projectgit/A.java
@@ -0,0 +1,16 @@
+package gitrepository.projectgit;
+
+import org.testng.annotations.Test;
+
+public class A {
+	@Test
+	public void login() {
+		System.out.println("login test");
+	}
+	@Test
+	public void logi2() {
+		System.out.println("login test2");
+	}
+
+
+}
